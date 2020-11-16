04a58cdb767306f7a181c8628cb53fd0830605fd
lukorir <37288440+lukorir@users.noreply.github.com>
2020-11-17 01:19:46 +0300

    Update mobile_phone_subscriptions.R

--- mobile_phone_subscriptions.R
+++ mobile_phone_subscriptions.R
@@ -9,8 +9,6 @@ setwd("G:/My Drive/MoreMilk/tidytuesday/Historical phones/tidytuesday-Historical
 mobile <- read.csv("mobile.csv")
 mob_dat <- mobile %>% filter(entity == "Libya" | entity == "Kenya" | entity == "Seychelles" , !is.na(mobile_subs), year>= 1995) 
 
-
-# The palette with black:
 cbp2 <- c("red", "green", "blue")
 
 p <-   ggplot(mob_dat, aes(x=year, y=mobile_subs, col = entity, group = entity)) +
@@ -23,26 +21,15 @@ p <-   ggplot(mob_dat, aes(x=year, y=mobile_subs, col = entity, group = entity))
        caption = "Data: OurWorldInData.org | plot@lukorir") +
   ylab("Mobile phone subscriptions per 100 people") +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 180)) +
-  #scale_y_continuous(breaks = seq(0, 175, by = 25)) +
   scale_x_continuous(breaks = seq(1995, 2017, by = 2)) +
   theme(plot.subtitle = element_text(lineheight=.5)) +
-  #theme(panel.background = element_rect(fill = "#fffbf5", color = NA)) +
   theme(plot.background = element_rect(fill = '#fffbf5')) +
   theme(axis.line.x = element_line(colour = 'black', size=0.5, linetype='solid'),
-        axis.line.y = element_line(colour = 'black', size=0.5, linetype='solid'))# +
-  #geom_vline(xintercept=c(2004), linetype="longdash", color = 'red')
+        axis.line.y = element_line(colour = 'black', size=0.5, linetype='solid'))
 
-
-# To use for line and point colors, add
 p <- p + scale_colour_manual(values=cbp2) +
   annotate(geom = "text", x = 2015,  y = 175,  label = "Seychelles", size= 4, fontface = "italic") +
   annotate(geom = "text", x = 2015,  y = 121,  label = "Libya", size= 4, fontface = "italic") +
   annotate(geom = "text", x = 2016,  y = 89,  label = "Kenya", size= 4, fontface = "italic") 
 
 ggsave("lby_syc_ken.png", p, width = 6, height = 4)
-  
-sendo <- image_read(paste0("sendo.jpg")) %>% 
-  image_resize(geometry_size_percent(5)) %>% 
-  image_background("#fffbf5")
-image_transparent(sendo, 'white')  
-
